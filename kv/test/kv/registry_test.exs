256e1d2a69865e3cabccc75f7e55f5c4c9463cc1
Angel Zambrano <angel.zguerrero@gmail.com>
2023-08-18 23:28:08 +0000

    Using dynamic supervisor for bucket spawning

--- kv/test/kv/registry_test.exs
+++ kv/test/kv/registry_test.exs
@@ -22,4 +22,17 @@ defmodule KV.RegistryTest do
     Agent.stop(bucket)
     assert KV.Registry.lookup(registry, "shopping") == :error
   end
+
+  test "removes bucket on crash", %{registry: registry} do
+    KV.Registry.create(registry, "shopping")
+    {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+
+    # Stop the bucket with non-normal reason
+    Agent.stop(bucket, :shutdown)
+    assert KV.Registry.lookup(registry, "shopping") == :error
+  end
+
+  test "are temporary workers" do
+    assert Supervisor.child_spec(KV.Bucket, []).restart == :temporary
+  end
 end
