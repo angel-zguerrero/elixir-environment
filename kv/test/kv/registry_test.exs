bb0d9be286e5b0024f187254c25586d91bfad23b
Angel Zambrano <angel.zguerrero@gmail.com>
2023-08-19 23:41:43 +0000

    horrible workaround to avoid race condition

--- kv/test/kv/registry_test.exs
+++ kv/test/kv/registry_test.exs
@@ -27,6 +27,7 @@ defmodule KV.RegistryTest do
     KV.Registry.create(registry, "shopping")
     {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
     Agent.stop(bucket)
+    _ = KV.Registry.create(registry, "bogus") # horrible workaround to avoid race condition
     assert KV.Registry.lookup(registry, "shopping") == :error
   end
 
@@ -40,6 +41,7 @@ defmodule KV.RegistryTest do
 
     # Stop the bucket with non-normal reason
     Agent.stop(bucket, :shutdown)
+    _ = KV.Registry.create(registry, "bogus") # horrible workaround to avoid race condition
     assert KV.Registry.lookup(registry, "shopping") == :error
   end
 
