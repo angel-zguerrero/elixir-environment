186c2d3e24ca51811dc06ebccd0461809fcab94d
Angel Zambrano <angel.zguerrero@gmail.com>
2023-08-27 13:21:43 +0000

    Testing connection loop

--- rkv_umbrella/apps/rkv_server/lib/rkv_server/tcp_server.ex
+++ rkv_umbrella/apps/rkv_server/lib/rkv_server/tcp_server.ex
@@ -4,8 +4,11 @@ defmodule RkvServer.TcpServer do
     {:ok, socket} = :gen_tcp.listen(port,
     [:binary, packet: :line, active: false, reuseaddr: true])
     Logger.info "Accepting connections on port #{port}"
+    accept_connection(socket)
+  end
 
+  def accept_connection(socket) do
     {:ok, client} = :gen_tcp.accept(socket)
-
+    accept_connection(socket)
   end
 end
